d9f01e456a5f3f0f03d29145cce2bf684b07db04
murugavallism <valli1211254@gmail.com>
2022-07-21 09:18:44 -0700

    vallibranch3

--- src/org/clonn/Clonn.java
+++ src/org/clonn/Clonn.java
@@ -1,7 +1,13 @@
 package org.clonn;
 
 public class Clonn {
+	 static int a=500;
+	 
 public static void main(String[] args) {
 	System.out.println("clone");
+}
+private  void vallibranch3() {
+	System.out.println("selvibranch3");
+
 }
 }
